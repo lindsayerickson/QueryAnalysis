00c3e73d651e0f691e4a0c0d218ccbff66e8f6c0
Lindsay Erickson <lerickson@wikimedia.org>
2026-02-27 15:37:42 -0500

    update placeholder paths in config.py

--- tools/config.py
+++ tools/config.py
@@ -1,8 +1,9 @@
-monthsFolder = "/home/mkroetzsch/querydata/months"
-queryReferenceDirectory = "/home/mkroetzsch/querydata/queryTypeReferenceFolder"
-fdupesExecutable = "/home/mkroetzsch/querydata/utility/fdupes"
-dbLocation = "/home/mkroetzsch/querydata/months/uniqueQueryMap.db"
-queryTypeMapDbLocation = "/home/mkroetzsch/querydata/months/queryTypeMap.db"
+# Edit these paths to make sense with your setup.
+monthsFolder = "/home/your-user/querydata/months"
+queryReferenceDirectory = "/home/your-user/querydata/queryTypeReferenceFolder"
+fdupesExecutable = "/home/your-user/querydata/utility/fdupes"
+dbLocation = "/home/your-user/querydata/months/uniqueQueryMap.db"
+queryTypeMapDbLocation = "/home/your-user/querydata/months/queryTypeMap.db"
 
 processedFolderName = "processedLogData/"
 sourceFolderName = "rawLogData/"
